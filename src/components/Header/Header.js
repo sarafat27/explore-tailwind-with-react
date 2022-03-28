64bc45039fd898dc29319b7aeb5a5b9fddbd2208
sarafat27 <sarafat2769@gmail.com>
2022-03-29 01:54:09 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl'>Save most of your money </h1>
+            <p>by giving your money to me</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, name: 'Home', path: '/home' },
+        { id: 1, name: 'Home', path: '/home' },
+        { id: 1, name: 'Home', path: '/home' },
+        { id: 1, name: 'Home', path: '/home' },
+    ]
+    return (
+        <nav>
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
